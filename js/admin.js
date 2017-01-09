f4e004b9c1dc286fa5e4eff0bd5634f44852eecc
David Taylor <david.taylor@avaaz.org>
2017-01-08 17:03:18 -0800

    Initial commit

---
+++ js/admin.js
@@ -0,0 +1,168 @@
+
+
+$(document).ready(function(){   
+
+//    Handlebars.registerPartial('translate_row', $('#translate_row').html());
+
+    Swag.registerHelpers(window.Handlebars);
+
+
+
+    var template = Handlebars.compile($("#groups-template").html());
+    $("#content").append(template(data));
+
+
+/*   MEMBERS */
+
+
+    $(".group_members-bulk_tags").hide();
+    $( ".group_members-bulk_tags--toggle").click(function() {
+        $(".group_members-bulk_tags").toggle();
+        return false;
+    });
+
+    $(".group_members-bulk_event").hide();
+    $( ".group_members-bulk_event--toggle").click(function() {
+        $(".group_members-bulk_event").toggle();
+        return false;
+    });
+
+    $(".group_members-bulk_activity").hide();
+    $( ".group_members-bulk_activity--toggle").click(function() {
+        $(".group_members-bulk_activity").toggle();
+        return false;
+    });
+
+    $(".group_members-bulk_score").hide();
+    $( ".group_members-bulk_score--toggle").click(function() {
+        $(".group_members-bulk_score").toggle();
+        return false;
+    });
+        
+
+    $(".group_member-row-notes").hide();
+    $( "#group_member-row-notes--toggle").click(function() {
+        $(".group_member-row-notes").toggle();
+        return false;
+    });
+
+
+
+
+
+
+
+
+    $( ".group-member-search_form" ).hide();
+    $( ".group-member-search_show" ).click(function() {
+        $( ".group-member-search_form" ).show();
+
+    });
+
+
+    $( ".group-member-search_item" ).hide();
+    $( ".group-member-search_criteria" ).change(function() {
+        $( ".group-member-search_item" ).hide();
+        $( ".group-member-search_scope" ).show();
+
+        var criteria = this.value;
+        
+        $( ".group-member-search_input" ).show();
+       $( ".group-member-search_search" ).show();
+       $( ".group-member-search_add" ).show();
+        
+       if (criteria == 'skill') {
+            $( ".group-member-search_input" ).show();
+           $(".group-member-search_input").attr("placeholder", "Select skills(s)");
+
+       }
+    
+       if (criteria == 'tag') {
+            $( ".group-member-search_input" ).show();
+           $(".group-member-search_input").attr("placeholder", "Select tag(s)");
+
+       }
+       if (criteria == 'group') {
+            $( ".group-member-search_input" ).show();
+           $(".group-member-search_input").attr("placeholder", "Select group(s)");
+
+       }
+       if (criteria =='geo') {
+            $( ".group-member-search_geo" ).show();
+            $( ".group-member-search_input" ).hide();
+            $( ".group-member-search_search" ).hide();
+            $( ".group-member-search_add" ).hide();
+
+        }
+
+       if (criteria == 'field') {
+           $( ".group-member-search_fields" ).show();    
+           $( ".group-member-search_operator" ).show();
+           $(".group-member-search_input").attr("placeholder", "Field value");
+           
+       }
+
+
+    });
+
+    $( ".group-member-search_geo" ).change(function() {
+        var criteria = this.value;
+       if (criteria == 'distance') {
+                 $( ".group-member-search_geo_distance" ).show();
+        }
+       if (criteria == 'country') {
+            $( ".group-member-search_input" ).show();
+            $(".group-member-search_input").attr("placeholder", "select country(s)");
+        }
+       if (criteria == 'state') {
+            $( ".group-member-search_input" ).show();
+            $(".group-member-search_input").attr("placeholder", "select state(s)");     
+        }
+       if (criteria == 'city') {
+            $( ".group-member-search_input" ).show();
+            $( ".group-member-search_operator" ).show();
+            $(".group-member-search_input").attr("placeholder", "select city"); 
+        }
+
+       if (criteria == 'point') {
+            $( ".group-member-search_geo_distance" ).show();
+            $( ".group-member-search_input" ).show();
+
+            $(".group-member-search_input").attr("placeholder", "enter lat/lng"); 
+        }
+        $( ".group-member-search_search" ).show();
+        $( ".group-member-search_add" ).show();
+
+    });
+
+
+
+/*   EVENTS */
+    
+
+    $( ".group_event-log_new_member_row--add").click(function() {
+        $(".group_event-log_new_member_row:last").after($('.group_event-log_new_member_row:last').clone());
+        return false;
+    });
+
+    $(".group_event-log_new_member").hide();
+    $( ".group_event-log_new_member--toggle").click(function() {
+        $(".group_event-log_new_member").toggle();
+        return false;
+    });
+
+
+    $(".event_list").hide();
+    $( ".event_list-toggle").click(function() {
+        $(".event_list").toggle();
+        return false;
+    });
+
+
+
+
+});   
+
+
+
+
---
+++ js/data.js
@@ -0,0 +1,274 @@
+var data={
+    "group_id":1,
+    "lang":"en",
+    "url":"/en/groups/oakland",
+    "group":{
+        "name":"East Bay Rising",
+        "city":"Oakland",
+        "region":"CA",
+        "country":"US",
+        "latitude":"37.8044",
+        "longitude":"-122.2708",
+        "truf_range":"10",
+        "turf_shape":[
+            "37.96260604160774, -122.42752075195312",
+            "37.90411590881245, -122.39593505859375",
+            "37.826056694926535, -122.34100341796875",
+            "37.74357118744906, -122.28469848632812",
+            "37.689254214025276, -122.22015380859375",
+            "37.73053874574077, -122.09518432617188",
+            "37.83148014503287, -122.08419799804688",
+            "37.95286091815649, -122.23251342773438"
+        ],
+        "contact_name":"David Taylor",
+        "contact_phone":"510-435-8920",
+        "contact_emails":"david@riseup.net",
+        "facebook_page_url":"http://facebook.com/Avaaz",
+        "stats":{
+            "member_count":10,
+            "recent_count":3,
+            "universe_count":5234            
+        },
+        "leaders":[
+            {
+                "user_id":"",
+                "name":"David Taylor",
+                "facebook_id":"511719841"
+            },
+            {
+                "user_id":"",
+                "name":"Annie Sartor",
+                "facebook_id":"507088373"
+            }            
+        ],
+        members:[
+            {
+                user_id:12345,
+                avatar:"",
+                first_name:"David",
+                last_name:"Taylor",
+                phone:"555-555-5555",
+                email:"name@gmail.com",
+                facebook_user_id:511719841,
+                twitter_name:"dtaylor",
+                activist_score:"4",
+                date_created:"2017-01-08",
+                date_updated:"2017-01-08",
+                group_admin:false,
+
+                tags:[
+                    {
+                        tag_id:"23",
+                        tag:"Puppy Lover"
+                    }  ,              
+                    {
+                        tag_id:"43",
+                        tag:"Eats Kittens"
+                    }  
+                ],
+                stats:{
+                    activity_count:0,
+                    event_count:1,
+                    date_last_active:"2016-12-23"
+                },
+                notes:[
+                    {
+                        id:"23434",
+                        note:"David tends to smell nice when he comes to meetings",
+                        date:"2017-01-02",
+                        logged_by_user:{
+                            id:1,
+                            name:'Davey T'   
+                        }      
+                    }
+                ],              
+                events:[
+                    {
+                        id:122,
+                        name:"Weekly Meeting",
+                        date:"2017-01-02",
+                        status:"rsvp"
+                    }
+                ],
+                activities:[
+                    {
+                        id:"",
+                        activity:"",
+                        date:"",
+                        logged_by_user:{
+                            id:1,
+                            name:'Davey T'   
+                        }
+                    }
+                ]
+            },
+            {
+                user_id:12345344,
+                avatar:"",
+                first_name:"Wombat",
+                last_name:"Hearder",
+                phone:"555-555-5555",
+                email:"wombat@gmail.com",
+                facebook_user_id:507088373,
+                twitter_name:"",
+                activist_score:"2",
+                date_created:"2017-01-08",
+                date_updated:"2017-01-08",
+                group_admin:false,
+
+                tags:[
+                    {
+                        tag_id:"18",
+                        tag:"Facilitator"
+                    }  ,              
+                    {
+                        tag_id:"78",
+                        tag:"NVDA Trainer"
+                    }  
+                ],
+                stats:{
+                    activity_count:0,
+                    event_count:1,
+                    date_last_active:"2016-12-23"
+                },
+                notes:[
+                    {
+                        id:"23434",
+                        note:"Very reliable but needs notice to take on tasks",
+                        date:"2017-01-02",
+                        logged_by_user:{
+                            id:1,
+                            name:'Davey T'   
+                        }      
+                    }
+                ],              
+                events:[
+                    {
+                        id:122,
+                        name:"Weekly Meeting",
+                        date:"2017-01-02",
+                        status:"rsvp"
+                    }
+                ],
+                activities:[
+                    {
+                        id:"",
+                        activity:"",
+                        date:"",
+                        logged_by_user:{
+                            id:1,
+                            name:'Davey T'   
+                        }
+                    }
+                ]
+            }
+        ],
+        nearby_groups:[
+            {
+                name:"San Francisco Actions",
+                group_id:"12",
+                url:"/en/groups/san_francsico",
+                distance:"24"
+            }
+        ],
+        events:[
+            {
+                id:1234,
+                date:"2017-02-01",
+                start_time:"19:30",
+                name:"Local Organzing Meeting",
+                description:"This is a great meeting you should come to",
+                location:{
+                    address:"1234 Main St",
+                    city:"Oakland",
+                    state:"CA",
+                    postal_code:"94610",
+                    country:"US",
+                    latitude:"",
+                    longitude:"",
+                    venue:"Meeting Room B, Union Hall",
+                    directions:"Buzz Union Local 12344 at the front door to be let in"
+                },
+                status:"public",
+                stats:{
+                    invited_count:12,
+                    rsvp_count:3,
+                    interested_count:2,
+                    declined_count:6,
+                    attended_count:9,
+                    not_attended_count:12
+                },
+                users:
+                    {
+                        invited: [
+                            {
+                                id:1233,
+                                first_name:"Bob",
+                                last_name:"Fishhead",
+                                facebook_user_id:"511719841",
+                                phone:"333-333-3333",
+                                date:"2017-01-01"
+                            } ,
+                            {
+                                id:123433,
+                                first_name:"Sam",
+                                last_name:"Fishhead",
+                                facebook_user_id:"511719841",
+                                phone:"333-333-3333",
+                                date:"2017-01-01"
+                            } 
+    
+                        ],
+                        rsvp:[
+                            {
+                                id:1233,
+                                first_name:"David",
+                                last_name:"Taylor",
+                                facebook_user_id:"",
+                                phone:"555-555-5555",
+                                date:"2017-01-01"
+                            },
+                            {
+                                id:1233,
+                                first_name:"Wombat",
+                                last_name:"Hearder",
+                                facebook_user_id:"",
+                                phone:"111-111-1111",
+                                date:"2017-01-01"
+                            }
+                        ]
+                    }
+
+                
+
+            }
+        ]
+    },
+    tags:[
+        {
+            id:1,
+            name:"Volunteer"
+        },
+        {
+            id:2,
+            name:"Kitten Lover"
+        },
+        {
+            id:3,
+            name:"Trainer"
+        },
+        {
+            id:4,
+            name:"Nerd"
+        },
+        {
+            id:5,
+            name:"Phone Bank"
+        }
+    ],
+    "admin_user":{
+        "user_id":"",
+        "name":"David Taylor",
+        "facebook_id":"511719841"        
+    }
+}
